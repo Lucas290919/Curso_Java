005c2b566f79ef36f688894869faa3ef2d77a931
Lucas290919 <106412804+Lucas290919@users.noreply.github.com>
2023-09-25 21:47:41 -0300

    Update Datateste.java

--- Aulas/src/classe/Datateste.java
+++ Aulas/src/classe/Datateste.java
@@ -1,16 +1,19 @@
 package classe;
-
-import java.util.Scanner;
-
 public class Datateste {
 public static void main(String[] args) {
 	
 	var d1 = new Data();
 	d1.dia = 23;
-	d1.mês = "Outubro";
+	d1.mês = 7;
 	d1.Ano = 2023;
 	
-	var 
+	System.out.printf("%d/%d/%d\n", d1.dia, d1.mês,d1.Ano);
+	
+	var d2 = new Data();
+	d2.dia = 10; 
+	d2.mês = 10;
+	d2.Ano = 11;
+	System.out.printf("%d/%d/%d", d2.dia, d2.mês,d2.Ano);
 	
 }
 }
